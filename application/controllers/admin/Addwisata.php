6534bf94d56ef687edd099618b9c27a75ca97622
kristt26 <kristt26@gmail.com>
2020-08-27 23:30:32 +0900

    ok

--- application/controllers/admin/Addwisata.php
+++ application/controllers/admin/Addwisata.php
@@ -8,22 +8,21 @@ class Addwisata extends CI_Controller
         parent::__construct();
         $this->load->model("admin/Wisata_model", "WisataModel");
         $this->load->model('admin/Kategori_model', 'KategoriModel');
-        if(!$this->session->userdata('user_data'))
-		{
-			redirect('auth');
-		}
+        if (!$this->session->userdata('user_data')) {
+            redirect('auth');
+        }
     }
 
     public function index()
     {
-        $Title = ['title'=>"Tambah Wisata", 'titledash'=>"Tambah Wisata"];
+        $Title = ['title' => "Tambah Wisata", 'titledash' => "Tambah Wisata"];
         $this->load->view('template/header', $Title);
         $this->load->view('admin/addwisata');
         $this->load->view('template/footer');
-        
+
     }
 
-    public function getdata($idwisata=null)
+    public function getdata($idwisata = null)
     {
         $data['wisata'] = $this->WisataModel->select();
         $data['kategori'] = $this->KategoriModel->select();
@@ -44,17 +43,16 @@ class Addwisata extends CI_Controller
             $data['foto'] = $file['file'];
         }
         $output = $this->WisataModel->insert($data);
-        if($output){
+        if ($output) {
             $this->session->set_flashdata('pesan', 'Berhasil di Tambahkan, success');
-            echo json_encode(["file"=>$file]);
-        }else{
+            echo json_encode(["file" => $file]);
+        } else {
             $this->session->set_flashdata('pesan', 'Berhasil di Tambahkan, error');
-            echo json_encode(["file"=>$file]);
+            echo json_encode(["file" => $file]);
         }
-        
-        
-	}
-	public function ubah()
+
+    }
+    public function ubah()
     {
         $data = $_POST;
         $cek = $this->WisataModel->selectfotowisata($data['idwisata']);
@@ -63,54 +61,38 @@ class Addwisata extends CI_Controller
         $output = $this->WisataModel->update($data);
         if ($output) {
             echo json_encode($data);
-        }else{
+        } else {
             echo json_encode(false);
         }
-	}
-	public function hapus()
+    }
+    public function hapus()
     {
         $data = $this->input->post();
-		$output = $this->WisataModel->delete($data);
-		$this->session->set_flashdata('error', 'Berhasil di Ubah');
-		redirect('admin/kategori');
-        
+        $output = $this->WisataModel->delete($data);
+        $this->session->set_flashdata('error', 'Berhasil di Ubah');
+        redirect('admin/kategori');
+
     }
-    public function upload($nilai=null)
+    public function upload()
     {
-        
-        $path_to_file = './assets/img/wisata/foto/' . $nilai->foto;
         $config['upload_path'] = './assets/img/wisata/foto/';
         $config['allowed_types'] = 'gif|jpg|png|jpeg|pdf';
         $config['max_size'] = 4096;
         $config['encrypt_name'] = true;
-        if(isset($_FILES['file'])){
-            if (isset($nilai->file)) {
-                if (unlink($path_to_file)) {
-                    $this->load->library('upload', $config);
-                    if ($this->upload->do_upload("file")) {
-                        $data = array('upload_data' => $this->upload->data());
-                        $image = $data['upload_data']['file_name'];
-                        // $result = $this->ProfileModel->updategambar($image);
-                        return array('file' => $image);
-                    }
-                } else {
-                    return [];
-                }
+        if (isset($_FILES['file'])) {
+            $this->load->library('upload', $config);
+            if ($this->upload->do_upload("file")) {
+                $data = array('upload_data' => $this->upload->data());
+                $image = $data['upload_data']['file_name'];
+                // $result = $this->ProfileModel->updategambar($image);
+                return array('file' => $image);
             } else {
-                $this->load->library('upload', $config);
-                if ($this->upload->do_upload("file")) {
-                    $data = array('upload_data' => $this->upload->data());
-                    $image = $data['upload_data']['file_name'];
-                    // $result = $this->ProfileModel->updategambar($image);
-                    return array('file' => $image);
-                }else{
-                    $error = array('error' => $this->upload->display_errors());
-                    $a=$error;
-                }
+                $error = array('error' => $this->upload->display_errors());
+                $a = $error;
             }
-        }else{
+        } else {
             return array('file' => $nilai->file);
         }
     }
-        
+
 }
